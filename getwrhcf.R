687b95fb978274f9df29849a7802e42b719f05b0
jaym <jmeder3@gmail.com>
2015-07-26 19:42:01 -0400

    Update for Knitr

--- getwrhcf.R
+++ getwrhcf.R
@@ -1,8 +1,10 @@
 # get wrhcashflow
+options(width = 150)
 library(dplyr)
 library(knitr)
-setwd("c:/code/rfiles")
+#setwd("c:/code/rfiles")
 #setwd("~/data")
+setwd("c:/artR")
 wrh <- read.csv("wrhcurrent.csv")
 wrh$pno <- as.factor(wrh$pno)
 wrh$st <- as.factor(wrh$st)
@@ -13,10 +15,12 @@ wrh$noift <- wrh$noiar/wrh$sqft
 rownames(wrh) <- wrh$pno
 #identify columns that can be totaled
 #these may change as you adjust the file
-totalcols <- c(units=6,rev=7,noiar=8,debtsvc=9,cflow=10,reno=11,corpexp=12,netcf=13,sqft=17)
+totalcols <- c(rev=7,noiar=8,debtsvc=9,cflow=10,reno=11,corpexp=12,netcf=13,sqft=17,units=6)
 #set up a vector with portfolio totals
 totals<-apply(wrh[,totalcols],2,sum)
-print (totals)
+print ("Portfolio Totals for Trailing 12 Months",quote=FALSE)
+print ("",quote = FALSE)
+print (format(totals,big.mark = ","),quote=FALSE)
 wrh$pctrev <- wrh$rev/totals["rev"]
 wrh$pctnoi <- wrh$noiar/totals["noiar"]
 wrh$pctunits <- wrh$units/totals["units"]
@@ -30,7 +34,7 @@ pctview <- c(2,7,8,26,27,28,29)
 genview <- c(2,4,7,8,10,15,6)
 debtview <-c(2,7,8,9,10,30,31,32,19,18)
 #head(wrh)
-str(wrh)
+#str(wrh)
 #groups for dplyr
 by_state <- group_by(wrh,st)
 by_area  <- group_by(wrh,loc)
@@ -39,4 +43,7 @@ by_lender <- group_by(wrh,lender)
 # Build states for filter
 states <- levels(wrh$st)
 for (i in states){
-p<-filter(wrh[,genview],wrh$st == i);p<-arrange(p,mkt);print(i);print(p,big.mark=",")}
+     p<-filter(wrh[,genview],wrh$st == i);p<-arrange(p,prop)
+     cat("State   :");print(i,quote = FALSE);print;print(format(p,justify="left",big.mark=","),print.gap=3)
+     sm <- format(colSums(p[,c(3,4,5,7)]),big.mark=","); print("Totals:",quote=FALSE);print(sm,quote="FALSE",print.gap=4);print("")}
+
