57a635094e2a48d9e404644b97d3457ed50336a9
jmeder <jmeder3@gmail.com>
2015-09-25 15:30:51 -0400

    some more

--- weekrpt.R
+++
@@ -1,8 +0,0 @@
-setwd("c:/code/R_files")
-options(scipen=4)
-status<- read.csv("weekreport.csv")
-str(status)
-library ("dplyr")
-by_reg <- group_by(status,regional)
-summarise(by_reg,runitr=sum(units))
-
